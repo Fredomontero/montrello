1d7012343d84c937ff544e808683efa8d2028c8b
fredomontero <christianmontero08@gmail.com>
2020-03-22 19:35:33 -0700

    setup proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,15 +1,15 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 // For development
-module.exports = function(app) {
-  app.use(
-    '/api',
-    createProxyMiddleware({
-      target: 'http://localhost:5500/graphql',
-      changeOrigin: true,
-    })
-  );
-};
+// module.exports = function(app) {
+//   app.use(
+//     '/api',
+//     createProxyMiddleware({
+//       target: 'http://localhost:5500/graphql',
+//       changeOrigin: true,
+//     })
+//   );
+// };
 
 // //For production
 // module.exports = function(app) {
